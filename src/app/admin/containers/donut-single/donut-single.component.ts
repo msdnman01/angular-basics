ff99540852b56f8d1dc7c410c737eadddf2c32e3
Michael Edwards <mgedwards@outlook.com>
2022-09-13 19:18:11 -0500

    lesson code

---
+++ src/app/admin/containers/donut-single/donut-single.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'donut-single',
+  templateUrl: 'donut-single.component.html',
+  styles: [
+  ]
+})
+export class DonutSingleComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}

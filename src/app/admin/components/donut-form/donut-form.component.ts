9291788f4623f5cde1c74155ec3c86de5acedfaf
Michael Edwards <mgedwards@outlook.com>
2022-09-28 16:25:44 -0500

    lesson code

---
+++ src/app/admin/components/donut-form/donut-form.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'donut-form',
+  templateUrl: 'donut-form.component.html',
+  styleUrls: ['donut-form.component.scss']
+})
+export class DonutFormComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}

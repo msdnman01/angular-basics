e590ef7e6e19780cd792727bf10db6a856913a73
Michael Edwards <mgedwards@outlook.com>
2022-10-09 23:48:38 -0500

    lesson code

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,8 +13,14 @@ export const routes: Routes = [
     path: 'admin',
     children: [
       { path: 'donuts', component: DonutListComponent },
-      { path: 'donut', component: DonutSingleComponent }
+      { path: 'donut', component: DonutSingleComponent },
+      { path: '', pathMatch: 'full', redirectTo: 'donuts' }
     ]
+  },
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'admin'
   }
 ];
 

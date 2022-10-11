b006171411406f904b36ea63ac909a94b2790e47
Michael Edwards <mgedwards@outlook.com>
2022-10-10 21:07:04 -0500

    lesson code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   standalone: true,
+  imports: [RouterModule],
   selector: 'app-root',
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.scss'],

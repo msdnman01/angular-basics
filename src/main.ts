f53824b2cd254d3760776dc2f2fdba535b697113
Michael Edwards <mgedwards@outlook.com>
2022-10-10 21:02:16 -0500

    lesson code

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,17 @@
-import { enableProdMode } from '@angular/core';
+import { importProvidersFrom, enableProdMode } from '@angular/core';
 import { bootstrapApplication } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
 
+import { AppRoutes } from './app/app.routes';
 import { AppComponent } from './app/app.component';
+
 import { environment } from './environments/environment';
 
 if (environment.production) {
   enableProdMode();
 }
 
-bootstrapApplication(AppComponent)
+bootstrapApplication(AppComponent, {
+  providers: [importProvidersFrom(RouterModule.forRoot(AppRoutes))]
+})
   .catch((err) => console.error(err));
